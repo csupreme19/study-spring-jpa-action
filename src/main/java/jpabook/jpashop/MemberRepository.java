b8712a9b53869622008c5f199157e05bc34ac711
csupreme19 <csupreme19@gmail.com>
2022-10-02 21:34:30 +0900

    기본 도메인 설정

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member findById(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public Member findByUsername(String username) {
+        return em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", username)
+                .getSingleResult();
+    }
+
+}
