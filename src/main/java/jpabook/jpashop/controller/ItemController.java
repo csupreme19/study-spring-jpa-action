3144de1401df2f9588fee226c970d773c9745a30
csupreme19 <csupreme19@gmail.com>
2022-10-12 00:53:16 +0900

    상품 등록, 목록

---
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.model.BookForm;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+
+        Book book = Book.builder()
+                .id(form.getId())
+                .name(form.getName())
+                .author(form.getAuthor())
+                .isbn(form.getIsbn())
+                .stockQuantity(form.getStockQuantity())
+                .price(form.getPrice())
+                .build();
+
+        itemService.addItem(book);
+
+        return "redirect:/items";
+    }
+
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+
+}
