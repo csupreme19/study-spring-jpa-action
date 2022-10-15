21ad4df1cbf715548ae4b0226843701ef992e42e
csupreme19 <csupreme19@gmail.com>
2022-10-15 13:34:26 +0900

    주문 조회 V5 - 메모리 DTO

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,6 +20,27 @@ public class OrderQueryRepository {
         List<OrderQueryDto> orders = findOrders();
         orders.forEach(o -> o.setOrderItems(findOrderItems(o.getOrderId())));
         return orders;
+    }
+
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> orders = findOrders();
+        orders.forEach(o -> o.setOrderItems(findOrderItemMap(toOrderIds(orders)).get(o.getOrderId())));
+        return orders;
+    }
+
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+        List<OrderItemQueryDto> orderItems = em.createQuery("select new jpabook.jpashop.model.OrderItemQueryDto(oi.order.id, i.name, oi.count, oi.orderPrice) " +
+                        "from OrderItem oi " +
+                        "join oi.item i " +
+                        "where oi.order.id in :orderIds", OrderItemQueryDto.class)
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+
+        return orderItems.stream().collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
+    }
+
+    private static List<Long> toOrderIds(List<OrderQueryDto> orders) {
+        return orders.stream().map(OrderQueryDto::getOrderId).toList();
     }
 
     private List<OrderItemQueryDto> findOrderItems(Long orderId) {
